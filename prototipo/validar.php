4279971fe8406a3b7791a60be60ab564404256ac
brunoandres <brunoandres2013@gmail.com>
2016-04-22 21:12:01 -0300

    Subo prototipo

---
+++ prototipo/validar.php
@@ -0,0 +1,21 @@
+<?php 
+
+$usuario=$_POST['user'];
+$clave=$_POST['password'];
+
+$user='admin';
+$pass='12345';
+
+if ($usuario==$user && $clave==$pass) {
+
+	echo 'Bievenido';
+	header('Location:principal.html');
+
+}else{
+
+	echo 'Error al conectar, verifique usuario y contrasenia';	
+}
+
+
+
+?>
