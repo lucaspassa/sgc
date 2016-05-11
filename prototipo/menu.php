8d7065c1420caa422b9b733cfe9cf083cba66706
MatthGitHub <matiasbenditti@gmail.com>
2016-05-11 19:41:27 -0300

    agregada pagina permisos

--- prototipo/menu.php
+++ prototipo/menu.php
@@ -58,7 +58,12 @@
             <li><a href="caja_consulta.php">Consultar Movimientos</a></li>
       </ul>
     </li>
-
+    <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Permisos<span class="caret"></span></a>
+      <ul class="dropdown-menu">
+        <li><a href="permisos.php">Consultar</a></li>
+        </ul>
+    </li>
     </ul>
   </div>
   <!-- /.navbar-collapse -->
