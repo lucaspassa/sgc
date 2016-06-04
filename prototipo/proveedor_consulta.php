58df2a06a46d6669b8bc66c984765c987af8c4d1
diegarces <diegogarcesbche@hotmail.com>
2016-06-03 21:09:13 -0300

    subo modificacion en proveedores consulta

--- prototipo/proveedor_consulta.php
+++ prototipo/proveedor_consulta.php
@@ -79,7 +79,7 @@
         <div class="jumbotron">
 
 
-            <div class="row">
+            <div class="table-responsive">
               <table class="table table-hover">
                   <thead>
                         <th> <input type="checkbox" value=""></th>
