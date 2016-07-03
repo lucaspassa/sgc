1005468817b92fa74762297bca03deab48749ca5
diegarces <diegogarcesbche@hotmail.com>
2016-07-02 21:50:24 -0300

    mas

--- cliente_alta.php
+++ cliente_alta.php
@@ -60,15 +60,15 @@
         
         <div class="input-group input-group-lg">
             <span class="input-group-addon" id="sizing-addon1"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span></span>
-            <input type="number" class="form-control" placeholder="CUIT" aria-describedby="sizing-addon1" required="" oninput="maxCuit(this)" maxlength="11">
+            <input type="number" class="form-control" placeholder="CUIT" aria-describedby="sizing-addon1" name="cuit" required="" oninput="maxCuit(this)" maxlength="11">
         </div><br/>
         <div class="input-group input-group-lg">
             <span class="input-group-addon" id="sizing-addon1"><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span></span>
-            <input type="text" class="form-control" placeholder="Dirección" aria-describedby="sizing-addon1" required="" maxlength="50">
+            <input type="text" class="form-control" placeholder="Dirección" aria-describedby="sizing-addon1" name="direccion" required="" maxlength="50">
         </div><br/>
         <div class="input-group input-group-lg">
             <span class="input-group-addon" id="sizing-addon1"><span class="glyphicon glyphicon-phone" aria-hidden="true"></span></span>
-            <input type="text" class="form-control" placeholder="Telefono" aria-describedby="sizing-addon1" required="" oninput="maxTel(this)" maxlength="20">
+            <input type="text" class="form-control" placeholder="Telefono" aria-describedby="sizing-addon1" name="telefono" required="" oninput="maxTel(this)" maxlength="20">
         </div><br/>
         <div class="input-group input-group-lg">
             <span class="input-group-addon" id="sizing-addon1"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span></span>
