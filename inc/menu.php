8f272aa109dc963f02c6431b5484effdfec6a542
diegarces <diegogarcesbche@hotmail.com>
2016-07-03 19:18:10 -0300

    cambio inicial en el menu

--- inc/menu.php
+++ inc/menu.php
@@ -14,19 +14,27 @@
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">  
+          <form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form>
+          
           <ul class="nav navbar-nav">
-            <li><a href="index.html">Cerrar sesion</a></li>
+            
             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Clientes<span class="caret"></span></a>
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user" aria-hidden="true"><span class="caret"></a>
               <ul class="dropdown-menu">
+              
                 <li><a href="/sgc/cliente_alta.php">Alta Cliente</a></li>
                 <li><a href="/sgc/cliente_consulta.php">Consultar Cliente</a></li>
               </ul>
             </li>
 
       <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ventas<span class="caret"></span></a>
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-signal" aria-hidden="true"><span class="caret"></span></a>
         <ul class="dropdown-menu">
         <li><a href="/sgc/ventas.php">Ventas realizadas</a></li>
         <li><a href="/sgc/reporte_venta.php">Generar Reporte</a></li>
@@ -34,7 +42,7 @@
       </li>
 
     <li class="dropdown">
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Proveedores<span class="caret"></span></a>
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"><span class="caret"></span></a>
       <ul class="dropdown-menu">
         <li><a href="/sgc/proveedor_alta.php">Alta Proveedor</a></li>
         <li><a href="/sgc/proveedor_consulta.php">Consultar Proveedor</a></li>
@@ -42,7 +50,7 @@
     </li>
 
     <li class="dropdown">
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Productos<span class="caret"></span></a>
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-glass" aria-hidden="true"><span class="caret"></span></a>
       <ul class="dropdown-menu">
         <li><a href="/sgc/producto_alta.php">Alta</a></li>
         <li><a href="/sgc/producto_consulta.php">Consultar</a></li>
@@ -50,7 +58,7 @@
     </li>
 
 	  <li class="dropdown">
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Caja<span class="caret"></span></a>
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-usd" aria-hidden="true"><span class="caret"></span></a>
       <ul class="dropdown-menu">
             <li><a href="/sgc/caja_ingreso.php">Ingreso</a></li>
             <li><a href="/sgc/caja_egreso.php">Egreso</a></li>
@@ -61,23 +69,21 @@
 
 
     <li class="dropdown">
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Usuarios<span class="caret"></span></a>
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span><span class="caret"></span></a>
       <ul class="dropdown-menu">
-            <li><a href="/sgc/usuario_alta.php">Alta usuario</a></li>
+            <li><a href="/sgc/usuario_alta.php">Nuevo usuario</a></li>
             <li><a href="/sgc/usuario_consulta.php">Consultar usuario</a></li>
+            <li><a href="/sgc/roles_alta.php">Nuevo Rol</a></li>
+          	<li><a href="/sgc/roles_consulta.php">Consultar Roles</a></li>
+
       </ul>
     </li>
 
-    <li class="dropdown">
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Roles<span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="/sgc/roles_alta.php">Alta</a></li>
-          <li><a href="/sgc/roles_consulta.php">Consultar</a></li>
-        </ul>
+        <li><a href="index.html"><span class="glyphicon glyphicon-off" aria-hidden="true"></span></a></li>
     </li>
 
     </ul>
-  </div>
+  
   <!-- /.navbar-collapse -->
 </div>
     <!-- /.container -->
