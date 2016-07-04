ea7e3f525d724789d34697eae913939ad503cba5
diegarces <diegogarcesbche@hotmail.com>
2016-07-03 23:26:58 -0300

    menu

--- inc/menu.php
+++ inc/menu.php
@@ -17,9 +17,9 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">  
           <form class="navbar-form navbar-left" role="search">
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="Search">
+              <input type="text" method="POST" class="form-control" placeholder="Search" action="buscar.php" >
             </div>
-            <button type="submit" class="btn btn-default">Submit</button>
+            <button type="submit" class="btn btn-default" name="busqueda" >Submit</button>
           </form>
           
           <ul class="nav navbar-nav">
