52cb7fd30e599421d6616019a137632200238688
MatthGitHub <matiasbenditti@gmail.com>
2016-07-06 18:07:46 -0300

    Agergo reporte ventas/ingresos

--- inc/reportes/generarReporte.php
+++ inc/reportes/generarReporte.php
@@ -6,32 +6,54 @@
 define('FPDF_FONTPATH', 'font/');
 require('mysql_table.php');
 include('DBConexionReportes.php');
+$tipo = $_GET['tipo'];
 
-class PDF extends PDF_MySQL_Table{
-    function Header()
-    {
-        // Titulo
-        $this->SetFont('Arial', '', 18);
-        $this->Cell(0, 6, 'Reporte de Usuarios', 0, 1, 'C');
-        $this->Ln(10);
-        // Asegurar la header de la tabla en el outpu
-        parent::Header();
-    }
+if($tipo == 'usuarios'){
+  class PDF extends PDF_MySQL_Table{
+      function Header()
+      {
+          // Titulo
+          $this->SetFont('Arial', '', 18);
+          $this->Cell(0, 6, 'Reporte de Usuarios', 0, 1, 'C');
+          $this->Ln(10);
+          // Asegurar la header de la tabla en el outpu
+          parent::Header();
+      }
+  }
+
+  $pdf=new PDF();
+  $pdf->AddPage();
+  //Primera tabla.
+  $pdf->Table('SELECT
+      	nombreUsuario,
+      	clave,
+      	fechaAlta,
+  	roles.nombreRol as rol
+      FROM
+  	   usuarios
+         INNER JOIN roles ON usuarios.idRol = roles.idRol');
+  ob_start();
+  $pdf->Output();
 }
 
-$pdf=new PDF();
-$pdf->AddPage();
-//Primera tabla.
-$pdf->Table('SELECT
-    	nombreUsuario,
-    	clave,
-    	fechaAlta,
-	roles.nombreRol as rol
-    FROM
-	   usuarios
-       INNER JOIN roles ON usuarios.idRol = roles.idRol');
-ob_start();
-$pdf->Output();
-?>
+if($tipo == 'ingresos'){
+  class PDF extends PDF_MySQL_Table{
+      function Header()
+      {
+          // Titulo
+          $this->SetFont('Arial', '', 18);
+          $this->Cell(0, 6, 'Reporte de Ventas', 0, 1, 'C');
+          $this->Ln(10);
+          // Asegurar la header de la tabla en el outpu
+          parent::Header();
+      }
+  }
 
+  $pdf=new PDF();
+  $pdf->AddPage();
+  //Primera tabla.
+  $pdf->Table('SELECT * FROM ingreso');
+  ob_start();
+  $pdf->Output();
+}
 ?>
